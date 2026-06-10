2361f8c22aa03fa4cb342fb9a4ab42e418015cf8
Tea_Key1 <ingethu2@gmail.com>
2026-06-11 01:32:44 +0900

    Add Xcode Cloud script beside workspace

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+
+cd "$ROOT_DIR"
+
+if [ -f package-lock.json ]; then
+  npm ci
+else
+  npm install
+fi
+
+cd ios
+pod install --repo-update
